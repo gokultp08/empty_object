cd9fb38ec904dd435650782c2705edec3b585120
gokultp08 <38941024+gokultp08@users.noreply.github.com>
2024-04-03 21:26:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,21 @@ const isNil = (data) => {
 const isEmptyObject = (obj) => {
     if (!isNil(obj) && typeof (obj) === 'object') {
         const keys = Object.keys(obj)
-        // console.log('keys', keys)
         if (keys.length > 0) {
             for (const [index, element] of keys.entries()) {
-                // console.log('inside keys loop', obj[element], typeof (obj[element]))
                 if (!isNil(obj[element])) {
                     if (typeof (obj[element]) === 'string') {
-                        // console.log('type string', obj[element])
                         if (!isEmptyString(obj[element])) return false
                     } else if (typeof (obj[element]) === 'number') {
-                        // console.log('type number', obj[element])
                         if (obj[element] > 0) {
                             return false
                         }
                     } else if (typeof (obj[element]) === 'array') {
                         const arr = [...obj[element]]
-                        // console.log('type array', arr)
                         if (arr.length > 0) {
                             if (!isEmptyArray(arr)) return false
                         }
                     } else if (typeof (obj[element]) === 'object') {
-                        // console.log('type obj', obj[element])
                         if (!isEmptyObject(obj[element])) return false
                     }
                 }
